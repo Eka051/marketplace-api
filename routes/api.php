a7d94707ee0b151da8491e9ba84dff2b14a46d06
Eka051 <dianekaraharjo@gmail.com>
2026-01-25 21:22:04 +0700

    feat: add protected and unprotected routes for BrandController; implement bulk store and resource routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BrandController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ShopController;
 use Illuminate\Support\Facades\Route;
@@ -22,9 +23,12 @@ Route::middleware(['auth:sanctum', 'role:seller', 'throttle:10,1'])->group(funct
     Route::post('shop', [ShopController::class, 'store']);
 });
 
+// Protected routes (requires authentication) -> Admin
 Route::middleware(['auth:sanctum', 'role:admin', 'throttle:10,1'])->group(function () {
     Route::apiResource('categories', CategoryController::class)->only(['store', 'update', 'destroy']);
     Route::post('categories/bulk', [CategoryController::class, 'bulkStore']);
+    Route::apiResource('brands', BrandController::class)->only(['store', 'update', 'destroy']);
+    Route::post('brands/bulk', [BrandController::class, 'bulkStore']);
 });
 
 // Unprotected routes (without authentication)
@@ -32,4 +36,5 @@ Route::middleware('throttle:10,1')->group(function () {
     Route::apiResource('products', ProductController::class)->only(['index', 'show']);
     Route::get('products/search', [ProductController::class, 'search']);
     Route::apiResource('categories', CategoryController::class)->only(['index', 'show']);
+    Route::apiResource('brands', BrandController::class)->only(['index', 'show']);
 });
