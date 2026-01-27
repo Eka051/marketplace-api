92d11a9516abb0f950ab40e5dcbfb3bbad6c8f97
Eka051 <dianekaraharjo@gmail.com>
2026-01-27 21:58:07 +0700

    feat: add UserController routes for user index and destroy actions in admin middleware

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BrandController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ShopController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // Authentication routes
@@ -25,6 +26,7 @@ Route::middleware(['auth:sanctum', 'role:seller', 'throttle:10,1'])->group(funct
 
 // Protected routes (requires authentication) -> Admin
 Route::middleware(['auth:sanctum', 'role:admin', 'throttle:10,1'])->group(function () {
+    Route::apiResource('users', UserController::class)->only(['index', 'destroy']);
     Route::apiResource('categories', CategoryController::class)->only(['store', 'update', 'destroy']);
     Route::post('categories/bulk', [CategoryController::class, 'bulkStore']);
     Route::apiResource('brands', BrandController::class)->only(['store', 'update', 'destroy']);
